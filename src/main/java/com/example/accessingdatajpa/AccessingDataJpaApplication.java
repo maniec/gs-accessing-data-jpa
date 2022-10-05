751788c682bc910aa5a4971072c59c741f7876d2
gschipper <gschipper@gmail.com>
2022-10-05 11:13:25 +0200

    refactoring to lambdas and streams

--- src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
+++ src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
@@ -7,52 +7,51 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class AccessingDataJpaApplication {
 
-	public static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
+    public static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccessingDataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataJpaApplication.class, args);
+    }
 
-	@Bean
-	// Returns a CommandLineRunner bean that automatically runs the code when the application launches
-	public CommandLineRunner runDemo(CustomerRepository repository) {
-		// First, it fetches the CustomerRepository from the Spring application context.
-		return args -> {
-// save a few customers
-			repository.save(new Customer("Jack", "Bauer"));
-			repository.save(new Customer("Chloe", "O'Brian"));
-			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
-			repository.save(new Customer("Michelle", "Dessler"));
+    @Bean
+    // Returns a CommandLineRunner bean that automatically runs the code when the application launches
+    public CommandLineRunner runDemo(CustomerRepository repository) {
+        // First, it fetches the CustomerRepository from the Spring application context.
+        return args -> {
+            // save a few customers
+            List.of(new Customer("Jack", "Bauer"),
+                            new Customer("Chloe", "O'Brian"),
+                            new Customer("Kim", "Bauer"),
+                            new Customer("David", "Palmer"),
+                            new Customer("Michelle", "Dessler")
+                    )
+                    .stream()
+                    .forEach(customer -> repository.save(customer));
 
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (Customer customer : repository.findAll()) {
-				log.info(customer.toString());
-			}
-			log.info("");
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            Stream.of(repository.findAll()).forEach(customer -> log.info(customer.toString()));
+            log.info("");
 
-			// fetch an individual customer by ID
-			Customer customer = repository.findById(1L);
-			log.info("Customer found with findById(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
+            // fetch an individual customer by ID
+            Customer customer = repository.findById(1L);
+            log.info("Customer found with findById(1L):");
+            log.info("--------------------------------");
+            log.info(customer.toString());
+            log.info("");
 
-			// fetch customers by last name
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			repository.findByLastName("Bauer").forEach(bauer -> {
-				log.info(bauer.toString());
-			});
-			// for (Customer bauer : repository.findByLastName("Bauer")) {
-			//  log.info(bauer.toString());
-			// }
-			log.info("");
-		};
-	}
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            repository.findByLastName("Bauer").forEach(bauer -> log.info(bauer.toString()));
+            log.info("");
+        };
+    }
 }
